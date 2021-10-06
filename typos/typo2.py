d82c4114e20196a01ef307ab2c039c9fba61968f
Bhargav <bhargavkadali39@gmail.com>
2021-10-06 09:23:02 +0530

    Update typo2.py

--- typos/typo2.py
+++ typos/typo2.py
@@ -1,7 +1,7 @@
 rows = 5
 
-for i in range(0,rows):
-    rows+=1
-    for j in range(0,i+1):
-        print("*",end="")
-    print("\r")
+for is i in rang(0,rows):
+    row+=1
+    for is j in rang(0,i+1):
+        print("*" end="")
+    print(\r)
