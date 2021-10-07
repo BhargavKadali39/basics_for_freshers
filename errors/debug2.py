22f6fcdcc2bf953298c9861575b0b5e2e671ccac
Sourcery AI <>
2021-10-07 14:54:16 +0000

    'Refactored by Sourcery'

--- errors/debug2.py
+++ errors/debug2.py
@@ -3,10 +3,10 @@
 
 def linear_Search(list1, n, key):  
 
-    # Searching list1 sequentially  
-    for i in range(0, n):  
+    # Searching list1 sequentially
+    for i in range(n):  
         if (list1[i] == key):  
-            return i  
+            return i
     return -1  
 
 
