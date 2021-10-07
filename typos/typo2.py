7bbadbff8e133f1052405160865515031ab18b52
Sourcery AI <>
2021-10-07 15:15:31 +0000

    'Refactored by Sourcery'

--- typos/typo2.py
+++ typos/typo2.py
@@ -1,7 +1,4 @@
-rows = 5
-
-for i in range(0,rows):
-    rows+=1
-    for j in range(0,i+1):
+for rows, i in enumerate(range(rows), start=5):
+    for _ in range(i+1):
         print("*", end="")
     print("\n")
