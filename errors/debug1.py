6f81deedd789df6c0ac63e37ecada6eff30bb7c9
praneethkontham <71930005+praneethkontham@users.noreply.github.com>
2021-10-07 20:21:28 +0530

    Update debug1.py

--- errors/debug1.py
+++ errors/debug1.py
@@ -1,9 +1,6 @@
 List_1 = [1,1,1,2,3,4,4,4,5,5,6,6]
 
+List_2
 
-List_2 = [];
-for i in List_1:
-  if i nothing in List_2:
-    List_2.append(i)
-    
-prints(List_2)
+List_2 = list(set(List_1))
+print(List_2)
