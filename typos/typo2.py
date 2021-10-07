0dba77a5a2b305b51b22eefa550540095bf88809
Aman Kumar <amanofficial16@gmail.com>
2021-10-07 20:40:03 +0530

    Fixed typos in typos/typo2.py

--- typos/typo2.py
+++ typos/typo2.py
@@ -1,7 +1,7 @@
 rows = 5
 
-for  i in range(0,rows):
-    row+=1
-    for  j in range(0,i+1):
-        print("*" end="")
-    print(\r)
+for i in range(0,rows):
+    rows+=1
+    for j in range(0,i+1):
+        print("*", end="")
+    print("\n")
