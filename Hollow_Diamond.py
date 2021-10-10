f52d73cdbce1604a2fd21edcab129562df100864
Sourcery AI <>
2021-10-10 09:22:09 +0000

    'Refactored by Sourcery'

--- Hollow_Diamond.py
+++ Hollow_Diamond.py
@@ -1,7 +1,7 @@
 n=int(input(''))
 N=2*n
-for i in range(0,N-1):
-    for j in range(0,N-1):
+for i in range(N-1):
+    for j in range(N-1):
         if i>=n:
             i=(N-2)%i
         if j<n:
@@ -9,9 +9,8 @@ for i in range(0,N-1):
                 print('*',end='')
             else:
                 print(' ',end='')
+        elif i==j%n+1:
+            print('*',end='')
         else:
-            if i==j%n+1:
-                print('*',end='')
-            else:
-                print(' ',end='')
+            print(' ',end='')
     print('')
